710e88ecfe269b991a3fca4260217345ccec0c24
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-23 17:49:33 +0200

    Made player movement

--- Ninja vs. Pirates/Assets/Scripts/PlayerMovement.cs
+++ Ninja vs. Pirates/Assets/Scripts/PlayerMovement.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
+    public float moveSpeed = 5;
+    public float rotationSpeed = 100;
+
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +13,29 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
 	}
+
+    void FixedUpdate()    {
+        Move();
+        Turn();
+    }
+
+    public void Move()    {
+        if (Input.GetKey(KeyCode.W))    {
+            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, - moveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Turn()    {
+        if (Input.GetKey(KeyCode.D))    {
+            transform.Rotate(0,rotationSpeed*Time.deltaTime,0);
+        }
+        if (Input.GetKey(KeyCode.A))    {
+            transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+        }
+    }
 }
