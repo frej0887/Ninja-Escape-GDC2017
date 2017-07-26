d762a99bf4da57099a43a3f6019484658842a41e
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-26 19:48:22 +0200

    Stuff I am not quite sure what is, but hey sure...

---
+++ Ninja vs. Pirates/Assets/Scripts/ShowTimerScore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ShowTimerScore : MonoBehaviour {
+    public TimerScript showTimer;
+    public Text timerScore;
+	// Use this for initialization
+	void Start () {
+        timerScore.text = "Your Time: " + showTimer.currentTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
