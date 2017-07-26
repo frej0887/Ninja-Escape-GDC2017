b3dea7c3f8c73da8e33cad7ccc5ebc50bd6e2be2
AIT-UNF-GDC <AIT-UNF-GDC@win.dtu.dk>
2017-07-27 00:01:14 +0200

    ikke noget nyt

---
+++ Ninja vs. Pirates/Assets/Scripts/SlipperyFloorScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlipperyFloorScript : MonoBehaviour {
+    public GameObject player;
+    public float pushBackDistance;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other) {
+        if(other.tag == "Player") {
+            print("SlipperyFloor");
+               if(Vector3.Distance(transform.position, player.transform.position) > pushBackDistance ) {
+                player.GetComponent<Pl>();
+               }
+
+        }
+
+    }
+
+    void OnTriggerExit() {
+
+
+    }
+
+
+}
