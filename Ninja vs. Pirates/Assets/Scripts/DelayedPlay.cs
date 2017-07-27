1ab065a5f700adfde56f340e1f2f1380f8820e15
Prog <Prog@gr2.gdc>
2017-07-28 00:05:39 +0200

    DONE ^-^ :D

---
+++ Ninja vs. Pirates/Assets/Scripts/DelayedPlay.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DelayedPlay : MonoBehaviour
+{
+
+    public AudioSource Audio;
+
+    public int i = 0;
+    public int maxtime;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+
+        if (i > maxtime)
+        {
+
+            Audio.Play();
+
+        }
+    }
+}
