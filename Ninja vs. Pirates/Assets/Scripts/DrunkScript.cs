13a8da9071d187631dfdd1d7e9b4c9aecef0a10c
Prog <Prog@gr2.gdc>
2017-07-27 19:31:11 +0200

    arbejder på animation til flaske

---
+++ Ninja vs. Pirates/Assets/Scripts/DrunkScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrunkScript : MonoBehaviour {
+    public Animator flask;
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void onTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            //flask.Play("Fall");
+            print("Yo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        }
+    }
+
+}
